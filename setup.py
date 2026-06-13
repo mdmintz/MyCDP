f898eedb33edad90136c356a182e0c6c90caa318
Michael Mintz <mdmintz@gmail.com>
2026-06-13 01:19:46 -0400

    Update PyPI classifiers

--- setup.py
+++ setup.py
@@ -105,6 +105,7 @@ setup(
         "Programming Language :: Python :: 3.12",
         "Programming Language :: Python :: 3.13",
         "Programming Language :: Python :: 3.14",
+        "Programming Language :: Python :: 3.15",
         "Topic :: Internet",
         "Topic :: Scientific/Engineering",
         "Topic :: Software Development",
